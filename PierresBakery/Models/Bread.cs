b202d061882c7c49fa5d30943dc656c149bc8567
Will W <wjwat@onionslice.org>
2022-02-25 11:07:28 -0600

    Pass test setting name of Bread object

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -3,9 +3,10 @@ namespace PierresBakery.Models
   public class Bread
   {
     public string Name { get; init; }
+
     public Bread(string name)
     {
-      // Do something here
+      Name = name;
     }
   }
 }
