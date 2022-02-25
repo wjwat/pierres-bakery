ec00bb4f548827ac3e9fc4a490f5958122f2e5ab
Will W <wjwat@onionslice.org>
2022-02-25 11:15:33 -0600

    Pass test to set amount for Bread object

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -3,11 +3,12 @@ namespace PierresBakery.Models
   public class Bread
   {
     public string Name { get; init; }
-    public int Amount { get; init; }
+    public int Amount { get; private set; }
 
     public Bread(string name, int amount)
     {
       Name = name;
+      Amount = amount;
     }
   }
 }
