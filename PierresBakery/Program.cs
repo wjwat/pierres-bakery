700350d3d5a0db0fdb4d6e34be4ef256d572f2cd
Will W <wjwat@onionslice.org>
2022-02-25 15:50:21 -0600

    WIP still plugging away at UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,8 +7,8 @@ namespace PierresBakery
 {
   public class Program
   {
-    public static List<Bread> DaysBread = new List<Bread>();
-    public static List<Pastry> DaysPastries = new List<Pastry>();
+    public static List<Item> Inventory = new List<Item>();
+    // public static List<Pastry> DaysPastries = new List<Pastry>();
 
     public static Dictionary<string, string> Messages = 
       new Dictionary<string, string>()
@@ -21,33 +21,59 @@ namespace PierresBakery
 
     public static void BuildBakery()
     {
-      DaysBread.AddRange(new List<Bread> {
+      Inventory.AddRange(new List<Bread> {
         new Bread("pan francés", 10),
         new Bread("pan de muerto", 5),
         new Bread("pan de yema", 7),
       });
+      // DaysPastries.AddRange(new List<Pastry> {
+      //   new Pastry("concha", 15),
+      //   new Pastry("gallina", 20),
+      //   new Pastry("empanada (pumpkin)", 8),
+      // });
     }
 
     public static string DisplayOptions(string message)
     {
-      for (int i = 0; i < DaysBread.Count; i++)
+      for (int i = 0; i < Inventory.Count; i++)
       {
-        Console.WriteLine($"[B{i}] {DaysBread[i].Name} ({DaysBread[i].Amount})");
+        string x = "".PadLeft(8) + $"[{i}] {Inventory[i].Name} ({Inventory[i].Amount})";
+        Console.WriteLine(x);
       }
+
+    //   Console.WriteLine("Pastries:".PadLeft(10));
+    //   for (int i = 0; i < DaysPastries.Count; i++)
+    //   {
+    //     int z = i+DaysBread.Count;
+    //     string x = "".PadLeft(8) + $"[{z}] {DaysPastries[i].Name} ({DaysPastries[i].Amount})";
+    //     Console.WriteLine(x);
+    //   }
 
       Console.Write(message);
 
       return Console.ReadLine();
     }
 
+    public static Dictionary<string, int> ParseSelection(
+      string sel,
+      Dictionary<string, int> Order)
+    {
+      if (sel[0] != 'B' || sel[0] != 'P')
+      {
+        Console.WriteLine("ERROR: Please enter a valid selection.");
+      }
+      return Order;
+    }
+
     public static void Main()
     {
-      BuildBakery();
       var Order = new Dictionary<string, int>();
+      BuildBakery();
+      Console.WriteLine(Messages["welcome"]);
       while (true)
       {
-        Console.WriteLine(Messages["welcome"]);
-        string x = DisplayOptions("\nWhat item would you like? > ");
+        string sel = DisplayOptions("\nWhat item would you like? >>> ");
+        Order = ParseSelection(sel.ToUpper(), Order);
       }
       Console.WriteLine(Messages["goodbye"].PadLeft(72, '.'));
     }
